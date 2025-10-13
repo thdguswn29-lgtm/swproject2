2fdd5d908ffe2a13e28af3ceb48806bc2f96462f
thdguswn29-lgtm <thdguswn29@gmail.com>
2025-10-13 13:04:15 +0900

    Update 11_practice_1.ino

--- 11_practice_1.ino
+++ 11_practice_1.ino
@@ -9,11 +9,7 @@
 #define _DIST_MIN 18
 #define _DIST_MAX 36
 
-float alpha = 0.2;
-
 Servo myservo;
-float dist_raw = 0;
-float dist_ema = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -24,40 +20,33 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(TRIG, LOW);
-  delayMicroseconds(2);
   digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(2);
   digitalWrite(TRIG, LOW);
+  delayMicroseconds(10);
+  digitalWrite(TRIG, HIGH);
 
-  long duration = pulseIn(ECHO, HIGH, 30000);
+  long duration = pulseIn(ECHO, HIGH);
   if (duration == 0) return;
 
-  dist_raw = duration * 0.034 / 2;
+  float distance = duration * 0.034 / 2;
+  Serial.println(distance);
 
-  if (dist_raw < _DIST_MIN || dist_raw > _DIST_MAX) {
-    digitalWrite(LED, HIGH);
-    return;
-  } else {
+  if (distance >= _DIST_MIN && distance <= _DIST_MAX) {
     digitalWrite(LED, LOW);
+  } else {
+    digitalWrite(LED, HIGH);
   }
 
-  dist_ema = alpha * dist_raw + (1 - alpha) * dist_ema;
-
   float angle;
-  if (dist_ema <= _DIST_MIN) angle = 0;
-  else if (dist_ema >= _DIST_MAX) angle = 180;
-  else angle = map(dist_ema, _DIST_MIN, _DIST_MAX, 0, 180);
+  if (distance <= _DIST_MIN) angle = 0;
+  else if (distance >= _DIST_MAX) angle = 180;
+  else angle = map(distance, _DIST_MIN, _DIST_MAX, 0, 180);
 
   int pulse = map(angle, 0, 180, _DUTY_MIN, _DUTY_MAX);
   myservo.writeMicroseconds(pulse);
 
-  Serial.print("Raw:");
-  Serial.print(dist_raw, 1);
-  Serial.print(", EMA:");
-  Serial.print(dist_ema, 1);
-  Serial.print(", Servo:");
-  Serial.println((int)angle);
-
   delay(50);
 }
+
+
